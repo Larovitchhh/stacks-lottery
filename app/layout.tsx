157f9c9488908d04d75aafd45c4f38a1672ac9e9
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-05 11:13:43 +0100

    Add metadata and change body background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'Stacks Lottery',
+  description: 'Built for Stacks Builder Rewards',
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#0f172a' }}>{children}</body>
     </html>
   )
 }
